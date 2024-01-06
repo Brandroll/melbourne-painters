ec1c74123399cc8e1d2fa99a639a282ace0b1694
mufaqar <mufaqar@gmail.com>
2024-01-06 19:56:57 +0500

    sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteUrl: process.env.SITE_URL || "https://example.com",
+  siteUrl: process.env.SITE_URL || "https://www.melbourne-painters.com.au",
   generateRobotsTxt: true,
   changefreq: "daily",
   priority: 0.7,
